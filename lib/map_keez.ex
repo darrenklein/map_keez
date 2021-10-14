6f148d8fb98084eda8e48f681002112fe8b82c38
Darren Klein <darren.a.klein@gmail.com>
2021-10-13 20:59:41 -0400

    Initial commit.

---
+++ lib/map_keez.ex
@@ -0,0 +1,18 @@
+defmodule MapKeez do
+  @moduledoc """
+  Documentation for `MapKeez`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> MapKeez.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/map_keez_test.exs
@@ -0,0 +1,8 @@
+defmodule MapKeezTest do
+  use ExUnit.Case
+  doctest MapKeez
+
+  test "greets the world" do
+    assert MapKeez.hello() == :world
+  end
+end
